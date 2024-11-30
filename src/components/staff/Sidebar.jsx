5c87556b0745ec6617628668e2e14546c5519e11
Rajesh Mondal <92754594+Developer-Rajesh100@users.noreply.github.com>
2024-11-30 23:29:42 +0530

    Staff panel's member list design done

--- src/components/staff/Sidebar.jsx
+++ src/components/staff/Sidebar.jsx
@@ -31,9 +31,8 @@ const Sidebar = ({ children }) => {
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center">
+      <div className="drawer-content flex flex-col m-5">
         {/* Page content here */}
-        {/* <Members /> */}
         {children}
 
         <label
